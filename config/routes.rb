bc82fc7669d06c7bf299c5013db51e6d942c77b8
Priyadharshini100301 <priyadharshiniraju10@gmail.com>
2022-10-27 15:25:12 +0530

    404

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
   
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+  
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+
   resources :jobs 
   resources :applies   
     # get'/', to: 'applies#accept', as: 'accept'
