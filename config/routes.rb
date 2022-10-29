64f4aad9f41db31e73e294fc398c21fb8c124857
Vasanthakumar <vasanthakumara117@gmail.com>
2022-10-29 21:02:16 +0530

    remodified the routes based on assosiation

--- config/routes.rb
+++ config/routes.rb
@@ -8,27 +8,20 @@ Rails.application.routes.draw do
   match "/404", to: "errors#not_found", via: :all
   match "/500", to: "errors#internal_server_error", via: :all
 
-  get '/applies/accepted', to: 'applies#accepted', as: 'accepted'
-  get '/applies/rejected', to: 'applies#rejected', as: 'rejected'
+  get '/jobs/:job_id/applies/rejected', to: 'applies#rejected', as: 'rejected'
+  get '/jobs/:job_id/applies/accepted', to: 'applies#accepted', as: 'accepted'
 
-  resources :jobs
-  resources :applies
-  post '/applies/:id/accept', to: 'applies#accept', as: 'accept'
-  post '/applies/:id/reject', to: 'applies#reject', as: 'reject'
-  # end
+  resources :jobs do
+    resources :applies
+  end
+  post '/jobs/:job_id/applies/:id/accept', to: 'applies#accept', as: 'accept'
+  post '/jobs/:job_id/applies/:id/reject', to: 'applies#reject', as: 'reject'
 
 
   root to: 'jobs#index'
-
-  # post '/applies', to: 'applies#accept', as: 'accept'
-  # post '/job_board1/apply', to: 'apply#reject', as: 'reject'
 
   devise_for :users, controllers: {registrations: 'registrations'}
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
-
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
