4d08cfed7ce68af65816f586847cfa0698de6b5e
Vasanthakumar <vasanthakumara117@gmail.com>
2022-10-29 21:02:56 +0530

    Refactored controller to take apply from job

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -1,37 +1,43 @@
 class AppliesController < ApplicationController
 
-        def index
-         @applies = Apply.all
-        end
+  def index
+    @job = Job.find(params[:job_id])
+  end
+
         def new
-          @apply =Apply.new
+          @job = Job.find(params[:job_id])
+          @apply = @job.applies.new()
         end
+
         def create
-          Apply.create(apply_params)
-          if @apply.nil?
+          @job = Job.find(params[:job_id])
+          @apply = @job.applies.new(apply_params)
+          if @apply.save
             redirect_to jobs_path,flash: {notice: "JOB IS APPLIED SUCCESSFULLY"}
          end
         end
         def show
-            @apply =  Apply.find(params[:id])
+          @apply =  Apply.find(params[:id])
         end
 
         def accepted
-          @applies = Apply.filter_by_accept
+          @job = Job.find(params[:job_id])
+          @applies = @job.applies.filter_by_accept
         end
 
         def rejected
-          @applies = Apply.filter_by_reject
+          @job = Job.find(params[:job_id])
+          @applies = @job.applies.filter_by_reject
         end
 
         def reject
           @apply = Apply.find(params[:id])
           @apply.status = "reject"
           if @apply.save
-            redirect_to applies_path and return
+            redirect_to job_applies_path and return
           end
           respond_to do |format|
-            format.html { redirect_to applies_path, notice: "The application is rejected" }
+            format.html { redirect_to job_applies_path, notice: "The application is rejected" }
             format.json { head :no_content }
           end
         end
@@ -40,10 +46,10 @@ class AppliesController < ApplicationController
           @apply = Apply.find(params[:id])
           @apply.status = "accept"
           if @apply.save
-            redirect_to applies_path and return
+            redirect_to job_applies_path and return
           end
           respond_to do |format|
-            format.html { redirect_to applies_path, notice: "application is accepted" }
+            format.html { redirect_to job_applies_path, notice: "application is accepted" }
             format.json { head :no_content }
           end
         end
