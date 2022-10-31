7cfc09cb6d3a31ac7e5fb39002d0e8f580503e14
Srinandhini M <srinandhini@sedintechnologies.com>
2022-10-31 12:24:33 +0530

    Validation is added for length of the title

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,7 +4,7 @@ class Job < ApplicationRecord
     belongs_to :user
     mount_uploader :avatar, AvatarUploader
     has_one_attached :resume
-    validates :title,presence: true
+    validates_length_of :title, maximum: 25, allow_blank: false
     validates :description,presence: true
     validates :url,presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
 
