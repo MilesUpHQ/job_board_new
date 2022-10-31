3239005058cc90cda6b9fb645905062648bfb683
Srinandhini M <srinandhini@sedintechnologies.com>
2022-10-31 20:45:08 +0530

    Code minimized using render

--- config/routes.rb
+++ config/routes.rb
@@ -2,20 +2,20 @@ require 'sidekiq/web'
 
 Rails.application.routes.draw do
 
-  get 'errors/not_found'
-  get 'errors/internal_server_error'
+   get 'errors/not_found'
+   get 'errors/internal_server_error'
 
-  match "/404", to: "errors#not_found", via: :all
-  match "/500", to: "errors#internal_server_error", via: :all
+   match "/404", to: "errors#not_found", via: :all
+   match "/500", to: "errors#internal_server_error", via: :all
 
-  get '/jobs/:job_id/applies/rejected', to: 'applies#rejected', as: 'rejected'
-  get '/jobs/:job_id/applies/accepted', to: 'applies#accepted', as: 'accepted'
+   get '/jobs/:job_id/applies/rejected', to: 'applies#rejected', as: 'rejected'
+   get '/jobs/:job_id/applies/accepted', to: 'applies#accepted', as: 'accepted'
 
   resources :jobs do
     resources :applies
   end
-  post '/jobs/:job_id/applies/:id/accept', to: 'applies#accept', as: 'accept'
-  post '/jobs/:job_id/applies/:id/reject', to: 'applies#reject', as: 'reject'
+   post '/jobs/:job_id/applies/:id/accept', to: 'applies#accept', as: 'accept'
+   post '/jobs/:job_id/applies/:id/reject', to: 'applies#reject', as: 'reject'
 
 
   root to: 'jobs#index'
